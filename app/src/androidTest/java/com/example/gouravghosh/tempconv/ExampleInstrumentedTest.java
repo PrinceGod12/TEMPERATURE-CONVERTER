a42d646e1b4ccb78c44418ace985f7518bbb09bb
GOURAV GHOSH <137384173+PrinceGod12@users.noreply.github.com>
2023-08-19 11:51:19 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/gouravghosh/tempconv/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/gouravghosh/tempconv/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.gouravghosh.tempconvdemo;
+package com.example.gouravghosh.tempconv;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.gouravghosh.tempconvdemo", appContext.getPackageName());
+        assertEquals("com.example.gouravghosh.tempconv", appContext.getPackageName());
     }
 }
